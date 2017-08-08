d674bc1bbc06b460af687db875d2f2e80f0a3821
Jonathan Bailey <bailey.jonathan@hotmail.com>
2017-08-07 22:56:06 -0500

    adding new files

--- provision_jenkins.sh
+++ provision_jenkins.sh
@@ -4,6 +4,10 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 echo "deb https://pkg.jenkins.io/debian binary/" >> /etc/apt/sources.list
 
 apt-get update
-apt-get install ansible -y
-echo "127.0.0.1" >> /etc/ansible/hosts
+apt-get install python-pip -y
+pip install ansible
+echo "localhost ansible_connection=local" >> /etc/ansible/hosts
 ansible-galaxy install geerlingguy.jenkins
+
+ansible-playbook -i "localhost," -c local /vagrant/jenkins_config.yml
+echo "jenkins   ALL=(ALL)   NOPASSWD:ALL" >> /etc/sudoers
